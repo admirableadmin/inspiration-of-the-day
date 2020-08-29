30455c711e4ae4a440265281dbf5849d4fb4c499
Andreas Peichert <andpei@users.noreply.github.com>
2020-08-29 11:26:28 +0200

    put quotes in separate file

---
+++ src/quotes.js
@@ -0,0 +1,21 @@
+const quotes = {
+    happiness: [
+        {
+            message: 'Hello Vue.js!',
+            author: 'demo',
+            day: '1-jan'
+        }, {
+            message: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.',
+            author: 'Lorem ipsum',
+            day: '1-feb'
+        }, {
+            message: 'Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.',
+            author: 'Lorem ipsum',
+            day: '15-aug'
+        }, {
+            message: 'Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. ',
+            author: 'Lorem ipsum',
+            day: '31-dec'
+        }
+    ]
+}
