1886587a9bdf6619962f33d37f1d59a29f57d77d
Andreas Peichert <admirableadmin@users.noreply.github.com>
2021-01-04 19:03:24 +0100

    add love quotes

--- src/quotes.js
+++ src/quotes.js
@@ -17,5 +17,24 @@ const quotes = {
             author: 'Lorem ipsum',
             day: '31-dec'
         }
+    ],
+    love: [
+        {
+            message: 'love Vue.js!',
+            author: 'love',
+            day: '1-jan'
+        }, {
+            message: 'love ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.',
+            author: 'love ipsum',
+            day: '1-feb'
+        }, {
+            message: 'love wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.',
+            author: 'love ipsum',
+            day: '15-aug'
+        }, {
+            message: 'love autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. ',
+            author: 'love ipsum',
+            day: '31-dec'
+        }
     ]
 }
