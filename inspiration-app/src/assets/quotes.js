ee757b599bf2adec14612863712d21d5ec970e8c
Andreas Peichert <admirableadmin@users.noreply.github.com>
2022-07-17 14:08:16 +0200

    add quotes.js to vue app

---
+++ inspiration-app/src/assets/quotes.js
@@ -0,0 +1,42 @@
+const quotes = {
+    happiness: [
+        {
+            message: 'Hello Vue.js!',
+            author: 'demo',
+            day: '1-jan'
+        }, {
+            message: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.',
+            author: 'Lorem ipsum',
+            day: '1-feb'
+        }, {
+            message: 'Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.',
+            author: 'Lorem ipsum',
+            day: '15-aug'
+        }, {
+            message: 'Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. ',
+            author: 'Lorem ipsum',
+            day: '31-dec'
+        }
+    ],
+    love: [
+        {
+            message: 'love Vue.js!',
+            author: 'love',
+            day: '1-jan'
+        }, {
+            message: 'love ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.',
+            author: 'Lorem ipsum',
+            day: '1-feb'
+        }, {
+            message: 'love wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.',
+            author: 'love ipsum',
+            day: '15-aug'
+        }, {
+            message: 'love autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. ',
+            author: 'love ipsum',
+            day: '31-dec'
+        }
+    ]
+}
+
+export default quotes
--- inspiration-app/src/views/HomeView.vue
+++ inspiration-app/src/views/HomeView.vue
@@ -1,11 +1,30 @@
 <script setup>
 import TheQuote from '@/components/TheQuote.vue'
 import { RouterLink } from 'vue-router'
+import quotes from '@/assets/quotes.js'
+</script>
+
+<script>
+export default {
+  data() {
+    return {
+      show: {author: "vuejs", message: "hello world", daylong: "today"}
+    }
+  },
+  methods: {
+  },
+  created() {
+    console.log(quotes);
+    this.show = quotes['happiness'][1];
+    this.show.daylong = new Date(this.show.day + new Date().getFullYear()).toLocaleString("de-de", {day: "numeric", month: "long", year: "numeric"});
+    console.log(this.show);
+  }
+}
 </script>
 
 <template>
   <article>
-    <TheQuote author="vuejs" message="hello world" daylong="today" />
+    <TheQuote :author="show.author" :message="show.message" :daylong="show.daylong" />
   </article>
   <nav>
     <RouterLink class="button prev" to="/" >&lt;&lt; past</RouterLink>
