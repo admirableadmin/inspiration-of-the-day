8e854db7e31583dc0bb7d6271d0a3630c84943ca
Andreas Peichert <admirableadmin@users.noreply.github.com>
2022-07-31 20:44:15 +0200

    remove console.log on production build * https://reactjsexample.com/a-vite-plugin-that-deletes-console-log-in-the-production-environment/

--- inspiration-app/vite.config.js
+++ inspiration-app/vite.config.js
@@ -2,10 +2,11 @@ import { fileURLToPath, URL } from 'url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import removeConsole from 'vite-plugin-remove-console';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(),removeConsole()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
